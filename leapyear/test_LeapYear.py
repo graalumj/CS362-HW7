8e2bf952b9a6f1d5d55eb665140c18525644d017
graalumj <graalumj@oregonstate.edu>
2021-05-31 13:59:48 -0700

    Update test_LeapYear.py
    test_LeapYear.py has been updated with a defined test for a year divisible by 4

--- leapyear/test_LeapYear.py
+++ leapyear/test_LeapYear.py
@@ -4,6 +4,8 @@ import unittest
 import LeapYear
 
 class test_leapyear(unittest.TestCase):
+    def test_four(self):
+        self.assertEqual(LeapYear.leapyear(2012), true)
     
 if __name__ == '__main__':
     unittest.main()
