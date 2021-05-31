c0ce5a9a850f4cef990257db3bd9a7c27a20a64f
graalumj <graalumj@oregonstate.edu>
2021-05-31 14:04:44 -0700

    Update test_LeapYear.py
    Updated true and false values of tests to match leap-year logic

--- leapyear/test_LeapYear.py
+++ leapyear/test_LeapYear.py
@@ -7,9 +7,9 @@ class test_leapyear(unittest.TestCase):
     def test_four(self):
         self.assertEqual(LeapYear.leapyear(2012), true)
     def test_hundred(self):
-        self.assertEqual(LeapYear.leapyear(2100), true)
+        self.assertEqual(LeapYear.leapyear(2100), false)
     def test_fourhundred(self):
-        self.assertEqual(LeapYear.leapyear(2000), false)
+        self.assertEqual(LeapYear.leapyear(2000), true)
     def test_normal(self):
         self.assertEqual(LeapYear.leapyear(2002), false)
     
