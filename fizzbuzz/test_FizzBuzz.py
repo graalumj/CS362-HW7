df4285bc7ed7f1707bc48d25b0769c8a8dc74821
graalumj <graalumj@oregonstate.edu>
2021-05-31 13:50:43 -0700

    Update test_FizzBuzz.py
    test_FizzBuzz.py has been updated with a defined test for a multiple of 15

--- fizzbuzz/test_FizzBuzz.py
+++ fizzbuzz/test_FizzBuzz.py
@@ -8,6 +8,8 @@ class test_FizzBuzz(unittest.TestCase):
         self.assertEqual(FizzBuzz.fizzbuzz(9), "Fizz")
     def test_five(self):
         self.assertEqual(FizzBuzz.fizzbuzz(20), "Buzz")
+    def test_fifteen(self):
+        self.assertEqual(FizzBuzz.fizzbuzz(45), "FizzBuzz")
     
 if __name__ == '__main__':
     unittest.main()
