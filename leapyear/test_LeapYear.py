1bd07f6e07dc157b316fe9a84ea0e478bf41275e
graalumj <graalumj@oregonstate.edu>
2021-05-31 14:00:34 -0700

    Update test_LeapYear.py
    test_LeapYear.py has been updated with a defined test for a year divisible by 100

--- leapyear/test_LeapYear.py
+++ leapyear/test_LeapYear.py
@@ -6,6 +6,8 @@ import LeapYear
 class test_leapyear(unittest.TestCase):
     def test_four(self):
         self.assertEqual(LeapYear.leapyear(2012), true)
+    def test_hundred(self):
+        self.assertEqual(LeapYear.leapyear(2100), true)
     
 if __name__ == '__main__':
     unittest.main()
