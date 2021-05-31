b6ed5fdc03a794ebf201ddef79f5d588d821945b
graalumj <graalumj@oregonstate.edu>
2021-05-31 13:48:58 -0700

    Update test_FizzBuzz.py
    test_FizzBuzz.py has been updated with a defined test for a multiple of 3

--- fizzbuzz/test_FizzBuzz.py
+++ fizzbuzz/test_FizzBuzz.py
@@ -4,6 +4,8 @@ import unittest
 import FizzBuzz
 
 class test_FizzBuzz(unittest.TestCase):
+    def test_three(self):
+        self.assertEqual(FizzBuzz.fizzbuzz(9), "Fizz")
     
 if __name__ == '__main__':
     unittest.main()
